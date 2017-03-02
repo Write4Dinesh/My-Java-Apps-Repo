41caf128fa1779a9d8dd6a3171f156c21b9567f3
Dinesh Masthaiah <dinesh.k.masthaiah@accenture.com>
2017-03-02 18:47:37 +0530

    Adjusted the alignments

--- AndroidApps/HelloWFMUpgrade/app/src/main/java/com/sg/hellowfmupgrade/WFMUpgradeActivity.java
+++ AndroidApps/HelloWFMUpgrade/app/src/main/java/com/sg/hellowfmupgrade/WFMUpgradeActivity.java
@@ -1,6 +1,8 @@
 package com.sg.hellowfmupgrade;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,13 @@ public class WFMUpgradeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upgrade);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         final Button gotoPlayStoreBtn = (Button) findViewById(R.id.goto_playstore_btn);
         gotoPlayStoreBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +31,12 @@ public class WFMUpgradeActivity extends Activity {
     }
 
     private void goToPlayStore() {
-        Toast.makeText(this, "Go to playstore button clicked", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Go to play store button clicked", Toast.LENGTH_LONG).show();
+        final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
     }
 }
