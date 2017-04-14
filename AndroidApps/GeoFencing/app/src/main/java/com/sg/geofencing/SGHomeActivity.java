0dc7d136b09b1be93d70036d5492eb871299fe8d
DineshMasthaiah <dinesh.masthaiah@wholefoods.com>
2017-04-14 21:18:18 +0530

    Fixed a build issue

--- AndroidApps/GeoFencing/app/src/main/java/com/sg/geofencing/SGHomeActivity.java
+++ AndroidApps/GeoFencing/app/src/main/java/com/sg/geofencing/SGHomeActivity.java
@@ -12,7 +12,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.ModernAsyncTask;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
