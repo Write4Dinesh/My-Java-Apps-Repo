5345f7a8cf434f51c3bf72f108b3bfc166a4b053
DineshMasthaiah <dinesh.masthaiah@wholefoods.com>
2017-04-18 14:52:50 +0530

    sample app to demo multiple receivers for same event

---
+++ AndroidApps/MultipleReceiverForSameEvent/app/src/main/java/com/sg/multiplereceiverforsameevent/BReceiver.java
@@ -0,0 +1,17 @@
+package com.sg.multiplereceiverforsameevent;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by dinesh.k.masthaiah on 4/18/2017.
+ */
+
+public class BReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("MultiReceiver", "Called onReceive of " + getClass().getSimpleName());
+    }
+}
