a0b7cc62644840a4450efeca57331112f5f7d549
DineshMasthaiah <dinesh.masthaiah@wholefoods.com>
2017-04-21 20:38:09 +0530

    Refactored the code

--- AndroidApps/GeoFencing/app/src/main/java/com/sg/geofencing/SGHomeActivity.java
+++ AndroidApps/GeoFencing/app/src/main/java/com/sg/geofencing/SGHomeActivity.java
@@ -39,35 +39,37 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SGHomeActivity extends Activity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        OnMapReadyCallback,
-        GoogleMap.OnMapClickListener,
-        GoogleMap.OnMarkerClickListener, LocationListener, ResultCallback {
-    private GoogleApiClient googleApiClient;
+public class SGHomeActivity extends Activity/* implements*/ /*GoogleApiClient.ConnectionCallbacks,*/
+        /*GoogleApiClient.OnConnectionFailedListener,*/
+        /*OnMapReadyCallback,*/
+       /* GoogleMap.OnMapClickListener*/
+        /*GoogleMap.OnMarkerClickListener, LocationListener, ResultCallback */ {
+    private GoogleApiClient mGoogleApiClient;
     public static final String TAG = "SGGeoFencing";
     public static final String MY_HOME_FENCE_ID = "my_home_fence";
-    private MapFragment mapFragment;
-    private GoogleMap map;
-    private TextView textLat, textLong;
-    private Location lastLocation;
+    private MapFragment mMapFragment;
+    private GoogleMap mGoogleMap;
+    private TextView mLatitudeTv, mLongitudeTv;
+    private Location mLastLocation;
     private static final int REQ_PERMISSION = 100;
-    private static final int GEOFENCE_RADIUS = 500;
+    private static final int GEO_FENCE_RADIUS = 500;
     private Geofence mMyHomeFence;
+    private LocationRequest mLocationRequest;
+    private Marker mGeoFenceMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sghome);
-        textLat = (TextView) findViewById(R.id.lat);
-        textLong = (TextView) findViewById(R.id.lon);
+        mLatitudeTv = (TextView) findViewById(R.id.lat);
+        mLongitudeTv = (TextView) findViewById(R.id.lon);
         createGoogleApi();
         long oneDay = System.currentTimeMillis() + 24 * 60 * 60 * 1000;
         int[] transition = new int[]{
                 Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT
         };
         //My Home : 12.9220761,77.4854912
-        mMyHomeFence = createGeoFence(MY_HOME_FENCE_ID, 12.9221894, 77.485419, GEOFENCE_RADIUS, oneDay, transition);
+        mMyHomeFence = createGeoFence(MY_HOME_FENCE_ID, 12.9221894, 77.485419, GEO_FENCE_RADIUS, oneDay, transition);
 
         // Create GoogleApiClient instance
         initGMaps();
@@ -80,10 +82,10 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
 
     private void createGoogleApi() {
         Log.d(TAG, "createGoogleApi()");
-        if (googleApiClient == null) {
-            googleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(new MyConnectionCallbacks())
+                    .addOnConnectionFailedListener(new MYOnConnectionFailedListener())
                     .addApi(LocationServices.API)
                     .build();
         }
@@ -91,8 +93,8 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
 
     // Initialize GoogleMaps
     private void initGMaps() {
-        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(new MyOnMapReadyCallback());
     }
 
     private Geofence createGeoFence(String fenceId, double latitude, double longitude, float radius, long expiryDurationInMillis, int[] transitionTypes) {
@@ -118,7 +120,7 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
         super.onStart();
 
         // Call GoogleApiClient connection when starting the Activity
-        googleApiClient.connect();
+        mGoogleApiClient.connect();
     }
 
     @Override
@@ -126,25 +128,18 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
         super.onStop();
 
         // Disconnect GoogleApiClient when stopping Activity
-        googleApiClient.disconnect();
-    }
-
-    // GoogleApiClient.ConnectionCallbacks connected
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Log.i(TAG, "onConnected()");
-        getLastKnownLocation();
+        mGoogleApiClient.disconnect();
     }
 
     // Get last known location
     private void getLastKnownLocation() {
         Log.d(TAG, "getLastKnownLocation()");
         if (checkPermission()) {
-            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-            if (lastLocation != null) {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
                 Log.i(TAG, "LasKnown location. " +
-                        "Long: " + lastLocation.getLongitude() +
-                        " | Lat: " + lastLocation.getLatitude());
+                        "Long: " + mLastLocation.getLongitude() +
+                        " | Lat: " + mLastLocation.getLatitude());
                 writeLastLocation();
                 startLocationUpdates();
             } else {
@@ -154,7 +149,7 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
         } else askPermission();
     }
 
-    private LocationRequest locationRequest;
+
     // Defined in mili seconds.
     // This number in extremely low, and should be used only for debug
     private final int UPDATE_INTERVAL = 1000;
@@ -163,22 +158,24 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
     // Start location Updates
     private void startLocationUpdates() {
         Log.i(TAG, "startLocationUpdates()");
-        locationRequest = LocationRequest.create()
+        mLocationRequest = LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(UPDATE_INTERVAL)
                 .setFastestInterval(FASTEST_INTERVAL);
 
         if (checkPermission())
-            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, new MyLocationListener());
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d(TAG, "onLocationChanged [" + location + "]");
-        lastLocation = location;
-        writeActualLocation(location);
-    }
-
+    /* @Override
+     public void onLocationChanged(Location location) {
+         Log.d(TAG, "onLocationChanged [" + location + "]");
+         mLastLocation = location;
+         mLatitudeTv.setText(getString(R.string.latitude) + mLastLocation.getLatitude());
+         mLongitudeTv.setText(getString(R.string.longitude) + mLastLocation.getLongitude());
+         writeActualLocation(location);
+     }
+ */
     // Asks for permission
     private void askPermission() {
         Log.d(TAG, "askPermission()");
@@ -197,27 +194,9 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
 
 
     private void writeLastLocation() {
-        writeActualLocation(lastLocation);
+        writeActualLocation(mLastLocation);
     }
 
-    // GoogleApiClient.ConnectionCallbacks suspended
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.w(TAG, "onConnectionSuspended()");
-    }
-
-    // GoogleApiClient.OnConnectionFailedListener fail
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.w(TAG, "onConnectionFailed()");
-    }
-
-    @Override
-    public void onMapClick(LatLng latLng) {
-        Log.d(TAG, "onMapClick(" + latLng + ")");
-        markerForGeofence(latLng);
-
-    }
 
     private PendingIntent geoFencePendingIntent;
     private final int GEOFENCE_REQ_CODE = 0;
@@ -246,7 +225,7 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
     // Start Geofence creation process
     private void startGeofence() {
         Log.i(TAG, "startGeofence()");
-        if (geoFenceMarker != null) {
+        if (mGeoFenceMarker != null) {
             Geofence geofence = mMyHomeFence;
             GeofencingRequest geofenceRequest = createGeofenceRequest(geofence);
             addGeofence(geofenceRequest);
@@ -265,11 +244,11 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
             geoFenceLimits.remove();
 
         CircleOptions circleOptions = new CircleOptions()
-                .center(geoFenceMarker.getPosition())
+                .center(mGeoFenceMarker.getPosition())
                 .strokeColor(Color.argb(50, 70, 70, 70))
                 .fillColor(Color.argb(100, 150, 150, 150))
-                .radius(GEOFENCE_RADIUS);
-        geoFenceLimits = map.addCircle(circleOptions);
+                .radius(GEO_FENCE_RADIUS);
+        geoFenceLimits = mGoogleMap.addCircle(circleOptions);
     }
 
     // Add the created GeofenceRequest to the device's monitoring list
@@ -277,10 +256,10 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
         Log.d(TAG, "addGeofence");
         if (checkPermission())
             LocationServices.GeofencingApi.addGeofences(
-                    googleApiClient,
+                    mGoogleApiClient,
                     request,
                     createGeofencePendingIntent()
-            ).setResultCallback(this);
+            ).setResultCallback(new MyResultCallback());
     }
 
     // Create a Geofence Request
@@ -292,20 +271,14 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
                 .build();
     }
 
-    @Override
+   /* @Override
     public boolean onMarkerClick(Marker marker) {
         Log.d(TAG, "onMarkerClickListener: " + marker.getPosition());
 
         return false;
     }
+*/
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Log.d(TAG, "onMapReady()");
-        map = googleMap;
-        map.setOnMapClickListener(this);
-        map.setOnMarkerClickListener(this);
-    }
 
     // Verify user's response of the permission requested
     @Override
@@ -347,18 +320,17 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
         MarkerOptions markerOptions = new MarkerOptions()
                 .position(latLng)
                 .title(title);
-        if (map != null) {
+        if (mGoogleMap != null) {
             // Remove the anterior marker
             if (locationMarker != null)
                 locationMarker.remove();
-            locationMarker = map.addMarker(markerOptions);
+            locationMarker = mGoogleMap.addMarker(markerOptions);
             float zoom = 14f;
             CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
-            map.animateCamera(cameraUpdate);
+            mGoogleMap.animateCamera(cameraUpdate);
         }
     }
 
-    private Marker geoFenceMarker;
 
     // Create a marker for the geofence creation
     private void markerForGeofence(LatLng latLng) {
@@ -369,22 +341,90 @@ public class SGHomeActivity extends Activity implements GoogleApiClient.Connecti
                 .position(latLng)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
                 .title(title);
-        if (map != null) {
+        if (mGoogleMap != null) {
             // Remove last geoFenceMarker
-            if (geoFenceMarker != null)
-                geoFenceMarker.remove();
+            if (mGeoFenceMarker != null)
+                mGeoFenceMarker.remove();
 
-            geoFenceMarker = map.addMarker(markerOptions);
+            mGeoFenceMarker = mGoogleMap.addMarker(markerOptions);
         }
     }
 
-    @Override
-    public void onResult(@NonNull Result result) {
-        Log.i(TAG, "onResult: " + result.getStatus());
-        if (result.getStatus().isSuccess()) {
-            drawGeofence();
-        } else {
-            // inform about fail
+    private class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d(TAG, "onLocationChanged [" + location + "]");
+            mLastLocation = location;
+            mLatitudeTv.setText(getString(R.string.latitude) + mLastLocation.getLatitude());
+            mLongitudeTv.setText(getString(R.string.longitude) + mLastLocation.getLongitude());
+            writeActualLocation(location);
+        }
+    }
+
+    private class MyMarkerClickListener implements GoogleMap.OnMarkerClickListener {
+
+        @Override
+        public boolean onMarkerClick(Marker marker) {
+            Log.d(TAG, "onMarkerClickListener: " + marker.getPosition());
+            return false;
+        }
+    }
+
+    private class MyResultCallback implements ResultCallback {
+        @Override
+        public void onResult(@NonNull Result result) {
+            Log.i(TAG, "onResult: " + result.getStatus());
+            if (result.getStatus().isSuccess()) {
+                drawGeofence();
+            } else {
+                // inform about fail
+            }
+        }
+    }
+
+    private class MyOnMapReadyCallback implements OnMapReadyCallback {
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            Log.d(TAG, "onMapReady()");
+            mGoogleMap = googleMap;
+            mGoogleMap.setOnMapClickListener(new MyOnMapClickListener());
+            mGoogleMap.setOnMarkerClickListener(new MyMarkerClickListener());
+        }
+    }
+
+    private class MYOnConnectionFailedListener implements GoogleApiClient.OnConnectionFailedListener {
+
+        // GoogleApiClient.OnConnectionFailedListener fail
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+            Log.w(TAG, "onConnectionFailed()");
+        }
+    }
+
+    private class MyConnectionCallbacks implements GoogleApiClient.ConnectionCallbacks {
+
+        // GoogleApiClient.ConnectionCallbacks connected
+        @Override
+        public void onConnected(@Nullable Bundle bundle) {
+            Log.i(TAG, "onConnected()");
+            getLastKnownLocation();
+        }
+
+        // GoogleApiClient.ConnectionCallbacks suspended
+        @Override
+        public void onConnectionSuspended(int i) {
+            Log.w(TAG, "onConnectionSuspended()");
+        }
+    }
+
+    private class MyOnMapClickListener implements GoogleMap.OnMapClickListener {
+        @Override
+        public void onMapClick(LatLng latLng) {
+            Log.d(TAG, "onMapClick(" + latLng + ")");
+            markerForGeofence(latLng);
+
         }
     }
 }
